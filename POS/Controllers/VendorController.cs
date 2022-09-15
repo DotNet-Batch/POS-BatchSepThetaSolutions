7dac341b5887f40e3a1c0a3758703b7803f41769
Toba Shaukat <toba.thetasolutions@gmail.com>
2022-09-15 13:35:55 +0500

    Lecture 4 > Viewbag, TenpData, RedirectToAction, Conditional Formatting

--- POS/Controllers/VendorController.cs
+++ POS/Controllers/VendorController.cs
@@ -6,14 +6,23 @@ namespace POS.Controllers
     {
         public IActionResult AddVendor()
         {
-            return View();
+            var firstName = "ABC";
+            var id = 0;
+            ViewBag.UserName = firstName;
+            ViewBag.Id = id;
+           // TempData["UserInfo"] = "Request From Add Vendor";
+            return RedirectToAction("AllVendors");
         }
         public IActionResult Index()
         {
+            ViewBag.UserName = "ASP.NET";
+            TempData["UserName"] = "ASP.NET";
+            ViewBag.Id = 1;
             return View();
         }
         public IActionResult AllVendors()
         {
+           ViewBag.Request = TempData["UserInfo"];
             return View();
         }
     }
